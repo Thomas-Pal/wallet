a791538a4eae56dd0218a5bc2116b1c5ce10c85f
ThomasPal <Thomas.Pal@ibm.com>
2025-11-14 13:18:01 +0000

    new

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wallet/', // 👈 repo name here
+  base: '/wallet/',   // matches repo name
 })
